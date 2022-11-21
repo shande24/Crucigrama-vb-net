ed0618d8817db71c5f55be7ba0d9aa5a0bb7a7e4
shande24 <81650327+shande24@users.noreply.github.com>
2022-11-21 16:06:31 -0300

    Add files via upload

---
+++ Crucigrama/Crucigrama.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Crucigrama
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonValidar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonValidar
+        '
+        Me.ButtonValidar.Location = New System.Drawing.Point(372, 12)
+        Me.ButtonValidar.Name = "ButtonValidar"
+        Me.ButtonValidar.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonValidar.TabIndex = 0
+        Me.ButtonValidar.Text = "VALIDAR"
+        Me.ButtonValidar.UseVisualStyleBackColor = True
+        '
+        'Crucigrama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 317)
+        Me.Controls.Add(Me.ButtonValidar)
+        Me.Name = "Crucigrama"
+        Me.Text = "CRUCIGRAMA"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ButtonValidar As System.Windows.Forms.Button
+
+End Class
---
+++ Crucigrama/Crucigrama.vb
@@ -0,0 +1,69 @@
+﻿Public Class Crucigrama
+    Dim TextBoxArray(10, 10) As TextBox
+    Const TxtBoxSize As Integer = 25
+    Dim crucigrama(,) As String = {
+        {"-", "-", "-", "-", "J", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "A", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "V", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "A", "-", "-", "-", "-", "-"},
+        {"T", "Y", "P", "E", "S", "C", "R", "I", "P", "T"},
+        {"-", "-", "-", "-", "C", "-", "-", "-", "Y", "-"},
+        {"-", "-", "-", "-", "R", "E", "A", "C", "T", "-"},
+        {"-", "-", "-", "-", "I", "-", "-", "-", "H", "-"},
+        {"-", "-", "-", "-", "P", "-", "-", "-", "O", "-"},
+        {"-", "N", "E", "X", "T", "-", "-", "-", "N", "-"}
+    }
+    Dim respuestas(,) As String
+
+
+    Private Sub LeerRespuestas()
+        Dim i, j
+        For i = 0 To 9
+            For j = 0 To 9
+                'Console.WriteLine("a")
+                respuestas(i, j) = TextBoxArray(i, j).Text
+            Next
+        Next
+    End Sub
+
+    Private Sub EvaluarRespuestas()
+        Dim i, j
+        Dim distinto As Boolean
+        For i = 0 To 9
+            For j = 0 To 9
+                If respuestas(i, j) <> crucigrama(i, j) Then
+                    distinto = True
+                End If
+            Next
+        Next
+        If distinto Then
+            MsgBox("Respuesta incorrecta")
+        Else
+            MsgBox("Respuesta correcta")
+        End If
+    End Sub
+
+    Private Sub Crucigrama_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i, j As Integer
+        For i = 0 To 9
+            For j = 0 To 9
+                TextBoxArray(i, j) = New TextBox
+                TextBoxArray(i, j).Width = TxtBoxSize
+                TextBoxArray(i, j).Height = TxtBoxSize
+                TextBoxArray(i, j).AutoSize = False
+                TextBoxArray(i, j).MaxLength = 1
+                Console.WriteLine(crucigrama(i, j))
+                If crucigrama(i, j) <> "-" Then
+                    Console.WriteLine("se crea una caja")
+                    TextBoxArray(i, j).Location = New Point(TxtBoxSize + j * TxtBoxSize, TxtBoxSize + i * TxtBoxSize)
+                    Me.Controls.Add(TextBoxArray(i, j))
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub ButtonValidar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonValidar.Click
+        LeerRespuestas()
+        EvaluarRespuestas()
+    End Sub
+End Class
